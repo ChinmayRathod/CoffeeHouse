698a7f38eb774c6478d0c9247e0a97a5470ad1c5
ChinmayRathod <chinmayrathod26@gmail.com>
2021-10-19 19:00:02 +0530

    v2
    added elements

---
+++ javascript/form.js
@@ -0,0 +1,10 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+  function closetyform() {
+    document.getElementById("tyForm").style.display = "none";
+  }
